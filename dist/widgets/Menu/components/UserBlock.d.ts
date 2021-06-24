a3e959cd994005aa6a993da15b49272223a019d6
Diogo Mariano <diogo.mariano@hitachivantara.com>
2021-06-24 22:01:51 +0100

    chore: remove dist folder

--- dist/widgets/Menu/components/UserBlock.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-interface Props {
-    account?: string;
-    login: any;
-    logout: () => void;
-}
-declare const _default: React.NamedExoticComponent<Props>;
-export default _default;
