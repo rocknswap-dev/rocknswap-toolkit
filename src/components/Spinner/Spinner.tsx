42b81137ddb0bcf3918ca38d74b3e90968acde70
Diogo Mariano <diogo.mariano@hitachivantara.com>
2021-10-02 20:06:30 +0100

    feat: update spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,52 +1,29 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
+import Logo from "./Logo";
 import { SpinnerProps } from "./types";
 
-const rotate = keyframes`
+const scale = keyframes`
   from {
-    transform: rotate(0deg);
+    transform: scale(1);
   }
   to {
-    transform: rotate(360deg);
+    transform: scale(1.3);
   }
-`;
-
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
 `;
 
 const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  animation: ${rotate} 2s linear infinite;
-  transform: translate3d(0, 0, 0);
+const RotatingLogo = styled(Logo)`
+  animation: ${scale} 2s alternate infinite ease-in;
 `;
 
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
-const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
+const Spinner: React.FC<SpinnerProps> = ({ size = 500 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <RotatingLogo width={`${size * 0.5}px`} />
     </Container>
   );
 };
